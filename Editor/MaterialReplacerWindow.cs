a6a915b247585c6313dd98978dc266b08a1b8eb3
AnosionV <scriptum_sacla@outlook.com>
2024-09-13 16:06:12 +0900

    ファイルスコープ名前空間はまだ使えなかった

--- Editor/MaterialReplacerWindow.cs
+++ Editor/MaterialReplacerWindow.cs
@@ -3,227 +3,229 @@ using UnityEditor;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Anosion.MaterialReplacer;
-
-public class MaterialReplacerWindow : EditorWindow
+namespace Anosion.MaterialReplacer
 {
-    /// <summary>
-    /// 検索対象のマテリアルリスト
-    /// </summary>
-    [SerializeField]
-    private List<Material> targetMaterials = new();
 
-    /// <summary>
-    /// 置き換え先のマテリアル
-    /// </summary>
-    private Material replaceMaterial;
-
-    private SerializedObject serializedObject;
-    private SerializedProperty targetMaterialsProperty;
-
-    /// <summary>
-    /// 検索結果を格納する辞書
-    /// </summary>
-    private Dictionary<GameObject, List<GameObject>> searchResults = new();
-
-    /// <summary>
-    /// ルートオブジェクトのトグル状態
-    /// </summary>
-    private Dictionary<GameObject, bool> toggles = new();
-
-    /// <summary>
-    /// 各オブジェクトごとのトグル状態
-    /// </summary>
-    private Dictionary<GameObject, bool> objectToggles = new();
-
-    private GUIStyle boxStyle;
-    private GUIStyle contentStyle;
-    private Vector2 scrollPosition;
-
-    [MenuItem("Window/Material Replacer")]
-    public static void ShowWindow()
+    public class MaterialReplacerWindow : EditorWindow
     {
-        GetWindow<MaterialReplacerWindow>("Material Replacer");
-    }
+        /// <summary>
+        /// 検索対象のマテリアルリスト
+        /// </summary>
+        [SerializeField]
+        private List<Material> targetMaterials = new();
 
-    private void OnEnable()
-    {
-        serializedObject = new SerializedObject(this);
-        targetMaterialsProperty = serializedObject.FindProperty("targetMaterials");
-    }
+        /// <summary>
+        /// 置き換え先のマテリアル
+        /// </summary>
+        private Material replaceMaterial;
 
-    private void OnGUI()
-    {
-        EditorGUILayout.LabelField("Search and Replace Materials", EditorStyles.boldLabel);
+        private SerializedObject serializedObject;
+        private SerializedProperty targetMaterialsProperty;
 
-        serializedObject.Update();
+        /// <summary>
+        /// 検索結果を格納する辞書
+        /// </summary>
+        private Dictionary<GameObject, List<GameObject>> searchResults = new();
 
-        // 検索対象のマテリアルリストが変更された場合に検索を再実行する
-        EditorGUI.BeginChangeCheck();
-        EditorGUILayout.PropertyField(targetMaterialsProperty, new GUIContent("Target Materials"), true);
+        /// <summary>
+        /// ルートオブジェクトのトグル状態
+        /// </summary>
+        private Dictionary<GameObject, bool> toggles = new();
 
-        // マテリアルリストが変更されたかチェック
-        if (EditorGUI.EndChangeCheck())
+        /// <summary>
+        /// 各オブジェクトごとのトグル状態
+        /// </summary>
+        private Dictionary<GameObject, bool> objectToggles = new();
+
+        private GUIStyle boxStyle;
+        private GUIStyle contentStyle;
+        private Vector2 scrollPosition;
+
+        [MenuItem("Window/Material Replacer")]
+        public static void ShowWindow()
         {
-            serializedObject.ApplyModifiedProperties();
+            GetWindow<MaterialReplacerWindow>("Material Replacer");
+        }
 
-            // マテリアルリストが空の場合は検索結果をクリアする
-            if (targetMaterials.Count == 0 || !targetMaterials.Any(material => material != null))
+        private void OnEnable()
+        {
+            serializedObject = new SerializedObject(this);
+            targetMaterialsProperty = serializedObject.FindProperty("targetMaterials");
+        }
+
+        private void OnGUI()
+        {
+            EditorGUILayout.LabelField("Search and Replace Materials", EditorStyles.boldLabel);
+
+            serializedObject.Update();
+
+            // 検索対象のマテリアルリストが変更された場合に検索を再実行する
+            EditorGUI.BeginChangeCheck();
+            EditorGUILayout.PropertyField(targetMaterialsProperty, new GUIContent("Target Materials"), true);
+
+            // マテリアルリストが変更されたかチェック
+            if (EditorGUI.EndChangeCheck())
             {
-                searchResults.Clear();
+                serializedObject.ApplyModifiedProperties();
+
+                // マテリアルリストが空の場合は検索結果をクリアする
+                if (targetMaterials.Count == 0 || !targetMaterials.Any(material => material != null))
+                {
+                    searchResults.Clear();
+                }
+                else
+                {
+                    SearchObjectsWithMaterials();
+                }
             }
-            else
+
+            // 置き換え先マテリアルを選択するフィールド
+            replaceMaterial = (Material)EditorGUILayout.ObjectField("Replace Material", replaceMaterial, typeof(Material), false);
+
+            if (searchResults.Count > 0)
             {
-                SearchObjectsWithMaterials();
+                InitializeStyles();
+                DisplaySearchResults();
+
+                // 置換ボタンの表示と有効化設定
+                GUI.enabled = replaceMaterial != null;
+                if (GUILayout.Button("Replace"))
+                {
+                    ReplaceMaterials();
+                }
+                GUI.enabled = true;
             }
         }
 
-        // 置き換え先マテリアルを選択するフィールド
-        replaceMaterial = (Material)EditorGUILayout.ObjectField("Replace Material", replaceMaterial, typeof(Material), false);
-
-        if (searchResults.Count > 0)
+        /// <summary>
+        /// 指定されたマテリアルを持つゲームオブジェクトを検索する
+        /// </summary>
+        private void SearchObjectsWithMaterials()
         {
-            InitializeStyles();
-            DisplaySearchResults();
+            if (targetMaterials.Count == 0) return;
 
-            // 置換ボタンの表示と有効化設定
-            GUI.enabled = replaceMaterial != null;
-            if (GUILayout.Button("Replace"))
-            {
-                ReplaceMaterials();
-            }
-            GUI.enabled = true;
+            // ヒエラルキー内のすべてのゲームオブジェクトを検索し、指定されたマテリアルを持つものを抽出する
+            searchResults = GameObject.FindObjectsOfType<GameObject>(true)
+                .Where(gameObject => gameObject.hideFlags == HideFlags.None)
+                .Where(gameObject => gameObject.GetComponent<Renderer>() != null)
+                .Where(gameObject => gameObject.GetComponent<Renderer>().sharedMaterials.Intersect(targetMaterials).Any())
+                .GroupBy(gameObject => gameObject.transform.root.gameObject)
+                .ToDictionary(group => group.Key, group => group.ToList());
+
+            // 各オブジェクトのトグル状態を初期化
+            toggles = searchResults.Keys.ToDictionary(key => key, key => true);
+            objectToggles = searchResults.Values.SelectMany(list => list).ToDictionary(obj => obj, obj => true);
         }
-    }
 
-    /// <summary>
-    /// 指定されたマテリアルを持つゲームオブジェクトを検索する
-    /// </summary>
-    private void SearchObjectsWithMaterials()
-    {
-        if (targetMaterials.Count == 0) return;
-
-        // ヒエラルキー内のすべてのゲームオブジェクトを検索し、指定されたマテリアルを持つものを抽出する
-        searchResults = GameObject.FindObjectsOfType<GameObject>(true)
-            .Where(gameObject => gameObject.hideFlags == HideFlags.None)
-            .Where(gameObject => gameObject.GetComponent<Renderer>() != null)
-            .Where(gameObject => gameObject.GetComponent<Renderer>().sharedMaterials.Intersect(targetMaterials).Any())
-            .GroupBy(gameObject => gameObject.transform.root.gameObject)
-            .ToDictionary(group => group.Key, group => group.ToList());
-
-        // 各オブジェクトのトグル状態を初期化
-        toggles = searchResults.Keys.ToDictionary(key => key, key => true);
-        objectToggles = searchResults.Values.SelectMany(list => list).ToDictionary(obj => obj, obj => true);
-    }
-
-    /// <summary>
-    /// GUIスタイルを初期化する
-    /// </summary>
-    private void InitializeStyles()
-    {
-        // タイトルのボックススタイル
-        boxStyle = new GUIStyle(EditorStyles.helpBox)
+        /// <summary>
+        /// GUIスタイルを初期化する
+        /// </summary>
+        private void InitializeStyles()
         {
-            margin = new RectOffset(10, 10, 5, 5),
-            padding = new RectOffset(10, 10, 10, 10)
-        };
+            // タイトルのボックススタイル
+            boxStyle = new GUIStyle(EditorStyles.helpBox)
+            {
+                margin = new RectOffset(10, 10, 5, 5),
+                padding = new RectOffset(10, 10, 10, 10)
+            };
 
-        // コンテンツのスタイル
-        contentStyle = new GUIStyle(EditorStyles.label)
+            // コンテンツのスタイル
+            contentStyle = new GUIStyle(EditorStyles.label)
+            {
+                margin = new RectOffset(20, 10, 5, 5),
+                padding = new RectOffset(10, 10, 5, 5)
+            };
+        }
+
+        /// <summary>
+        /// 検索結果を表示する
+        /// </summary>
+        private void DisplaySearchResults()
         {
-            margin = new RectOffset(20, 10, 5, 5),
-            padding = new RectOffset(10, 10, 5, 5)
-        };
-    }
+            // スクロールビューの開始
+            scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
 
-    /// <summary>
-    /// 検索結果を表示する
-    /// </summary>
-    private void DisplaySearchResults()
-    {
-        // スクロールビューの開始
-        scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
-
-        foreach (var (root, resultMeshes) in searchResults)
-        {
-            EditorGUILayout.Space();
-            EditorGUILayout.BeginVertical(boxStyle);
-
-            // ルートオブジェクトのトグルと表示
-            EditorGUILayout.BeginHorizontal();
-            toggles[root] = EditorGUILayout.Toggle(toggles[root], GUILayout.Width(15));
-            CreateReadonlyObjectField("", root);
-            EditorGUILayout.EndHorizontal();
-
-            if (toggles[root])
+            foreach (var (root, resultMeshes) in searchResults)
             {
                 EditorGUILayout.Space();
+                EditorGUILayout.BeginVertical(boxStyle);
 
-                // コンテンツの垂直レイアウト
-                EditorGUILayout.BeginVertical(contentStyle);
+                // ルートオブジェクトのトグルと表示
+                EditorGUILayout.BeginHorizontal();
+                toggles[root] = EditorGUILayout.Toggle(toggles[root], GUILayout.Width(15));
+                CreateReadonlyObjectField("", root);
+                EditorGUILayout.EndHorizontal();
 
-                foreach (var mesh in resultMeshes)
+                if (toggles[root])
                 {
-                    EditorGUILayout.BeginHorizontal();
-                    objectToggles[mesh] = EditorGUILayout.Toggle(objectToggles[mesh], GUILayout.Width(15));
-                    CreateReadonlyObjectField("", mesh);
-                    EditorGUILayout.EndHorizontal();
+                    EditorGUILayout.Space();
+
+                    // コンテンツの垂直レイアウト
+                    EditorGUILayout.BeginVertical(contentStyle);
+
+                    foreach (var mesh in resultMeshes)
+                    {
+                        EditorGUILayout.BeginHorizontal();
+                        objectToggles[mesh] = EditorGUILayout.Toggle(objectToggles[mesh], GUILayout.Width(15));
+                        CreateReadonlyObjectField("", mesh);
+                        EditorGUILayout.EndHorizontal();
+                    }
+
+                    EditorGUILayout.EndVertical();
                 }
 
                 EditorGUILayout.EndVertical();
             }
 
-            EditorGUILayout.EndVertical();
+            // スクロールビューの終了
+            EditorGUILayout.EndScrollView();
         }
 
-        // スクロールビューの終了
-        EditorGUILayout.EndScrollView();
-    }
-
-    /// <summary>
-    /// 選択されたオブジェクトのマテリアルを置き換える
-    /// </summary>
-    private void ReplaceMaterials()
-    {
-        // Undoグループの開始
-        Undo.IncrementCurrentGroup();
-        Undo.SetCurrentGroupName("Replace Materials");
-
-        // 検索結果の各オブジェクトのマテリアルを置き換える
-        var replaceTargetRenderer = searchResults
-            .Where(kv => toggles[kv.Key]) // トグルが true のオブジェクトのみに絞る
-            .SelectMany(kv => kv.Value)
-            .Where(obj => objectToggles[obj]) // 個々のオブジェクトのチェック状態も確認
-            .Select(obj => obj.GetComponent<Renderer>())
-            .Where(renderer => renderer != null);
-
-        foreach (var renderer in replaceTargetRenderer)
+        /// <summary>
+        /// 選択されたオブジェクトのマテリアルを置き換える
+        /// </summary>
+        private void ReplaceMaterials()
         {
-            // Rendererの状態をUndoの対象として記録
-            Undo.RecordObject(renderer, "Replace Material");
+            // Undoグループの開始
+            Undo.IncrementCurrentGroup();
+            Undo.SetCurrentGroupName("Replace Materials");
 
-            // マテリアルを置き換える
-            renderer.sharedMaterials = renderer.sharedMaterials
-                .Select(material => targetMaterials.Contains(material) ? replaceMaterial : material)
-                .ToArray();
+            // 検索結果の各オブジェクトのマテリアルを置き換える
+            var replaceTargetRenderer = searchResults
+                .Where(kv => toggles[kv.Key]) // トグルが true のオブジェクトのみに絞る
+                .SelectMany(kv => kv.Value)
+                .Where(obj => objectToggles[obj]) // 個々のオブジェクトのチェック状態も確認
+                .Select(obj => obj.GetComponent<Renderer>())
+                .Where(renderer => renderer != null);
+
+            foreach (var renderer in replaceTargetRenderer)
+            {
+                // Rendererの状態をUndoの対象として記録
+                Undo.RecordObject(renderer, "Replace Material");
+
+                // マテリアルを置き換える
+                renderer.sharedMaterials = renderer.sharedMaterials
+                    .Select(material => targetMaterials.Contains(material) ? replaceMaterial : material)
+                    .ToArray();
+            }
+
+            // Undoグループを一つの操作としてまとめる
+            Undo.CollapseUndoOperations(Undo.GetCurrentGroup());
+
+            // 検索を再実行して最新の状態を反映
+            SearchObjectsWithMaterials();
         }
 
-        // Undoグループを一つの操作としてまとめる
-        Undo.CollapseUndoOperations(Undo.GetCurrentGroup());
-
-        // 検索を再実行して最新の状態を反映
-        SearchObjectsWithMaterials();
-    }
-
-    /// <summary>
-    /// 読み取り専用のオブジェクトフィールドを作成する
-    /// </summary>
-    /// <param name="label">オブジェクトフィールドのラベル</param>
-    /// <param name="obj">表示するオブジェクト</param>
-    private void CreateReadonlyObjectField(string label, Object obj)
-    {
-        GUI.enabled = false;
-        EditorGUILayout.ObjectField(label, obj, typeof(GameObject), false);
-        GUI.enabled = true;
+        /// <summary>
+        /// 読み取り専用のオブジェクトフィールドを作成する
+        /// </summary>
+        /// <param name="label">オブジェクトフィールドのラベル</param>
+        /// <param name="obj">表示するオブジェクト</param>
+        private void CreateReadonlyObjectField(string label, Object obj)
+        {
+            GUI.enabled = false;
+            EditorGUILayout.ObjectField(label, obj, typeof(GameObject), false);
+            GUI.enabled = true;
+        }
     }
 }
