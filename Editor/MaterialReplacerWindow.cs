0b20ab373edeb770dd22b2d07729a7a97c179c53
Anosion <anosion.vr@gmail.com>
2024-10-29 16:46:55 +0900

    UI を作成

--- Editor/MaterialReplacerWindow.cs
+++ Editor/MaterialReplacerWindow.cs
@@ -1,52 +1,16 @@
-using UnityEngine;
 using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+using VRC.SDK3.Avatars.Components;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Anosion.MaterialReplacer
 {
-
     public class MaterialReplacerWindow : EditorWindow
     {
-        /// <summary>
-        /// 検索対象のマテリアルリスト
-        /// </summary>
-        [SerializeField]
-        private List<Material> targetMaterials = new();
-
-        /// <summary>
-        /// 置き換え先のマテリアル
-        /// </summary>
-        private Material replaceMaterial;
-
-        /// <summary>
-        /// 置換後に設定を更新するかどうかを決めるチェックボックス
-        /// </summary>
-        private bool switchMaterialsAfterReplace = false;
-
-        private SerializedObject serializedObject;
-        private SerializedProperty targetMaterialsProperty;
-
-        /// <summary>
-        /// 検索結果を格納する辞書
-        /// </summary>
-        private Dictionary<GameObject, List<GameObject>> searchResults = new();
-
-        /// <summary>
-        /// ルートオブジェクトのトグル状態
-        /// </summary>
-        private Dictionary<GameObject, bool> toggles = new();
-
-        /// <summary>
-        /// 各オブジェクトごとのトグル状態
-        /// </summary>
-        private Dictionary<GameObject, bool> objectToggles = new();
-
-        private Vector2 scrollPosition;
-
-        public GUIStyle BoxStyle { get; set; }
-
-        public GUIStyle ContentStyle { get; set; }
+        private Vector2 scrollPosition = Vector2.zero;
+        private List<VRCAvatarDescriptor> avatars = new List<VRCAvatarDescriptor>();
+        private ReorderableList avatarList;
 
         [MenuItem("Window/Material Replacer")]
         public static void ShowWindow()
@@ -56,193 +20,137 @@ namespace Anosion.MaterialReplacer
 
         private void OnEnable()
         {
-            serializedObject = new SerializedObject(this);
-            targetMaterialsProperty = serializedObject.FindProperty("targetMaterials");
+            SetupAvatarList();
+        }
 
-            BoxStyle = new GUIStyle();
-            ContentStyle = new GUIStyle()
+        private void SetupAvatarList()
+        {
+            avatarList = new ReorderableList(avatars, typeof(VRCAvatarDescriptor), true, true, true, true);
+            avatarList.drawHeaderCallback = (Rect rect) =>
             {
-                margin = new RectOffset(20, 0, 0, 0)
+                EditorGUI.LabelField(rect, "Avatars");
+            };
+
+            avatarList.drawElementCallback = (Rect rect, int index, bool isActive, bool isFocused) =>
+            {
+                avatars[index] = (VRCAvatarDescriptor)EditorGUI.ObjectField(
+                    new Rect(rect.x, rect.y, rect.width, EditorGUIUtility.singleLineHeight),
+                    avatars[index], typeof(VRCAvatarDescriptor), true);
+            };
+
+            avatarList.onAddCallback = (ReorderableList list) =>
+            {
+                avatars.Add(null);
+            };
+
+            avatarList.onRemoveCallback = (ReorderableList list) =>
+            {
+                avatars.RemoveAt(list.index);
             };
         }
 
         private void OnGUI()
         {
-            EditorGUILayout.LabelField("Search and Replace Materials", EditorStyles.boldLabel);
+            EditorGUILayout.BeginVertical();
+            GUILayout.Label("Material Replacer", EditorStyles.boldLabel);
 
-            serializedObject.Update();
+            Rect listRect = GUILayoutUtility.GetRect(0, avatarList.GetHeight(), GUILayout.ExpandWidth(true));
+            avatarList.DoList(listRect);
 
-            // 検索対象のマテリアルリストが変更された場合に検索を再実行する
-            EditorGUI.BeginChangeCheck();
-            EditorGUILayout.PropertyField(targetMaterialsProperty, new GUIContent("Target Materials"), true);
+            HandleDragAndDrop(listRect);
 
-            // マテリアルリストが変更されたかチェック
-            if (EditorGUI.EndChangeCheck())
+            EditorGUILayout.EndVertical();
+
+            scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition, GUILayout.Height(position.height - 100));
+            if (avatars.Count > 0)
             {
-                serializedObject.ApplyModifiedProperties();
-
-                // マテリアルリストが空の場合は検索結果をクリアする
-                if (targetMaterials.Count == 0 || !targetMaterials.Any(material => material != null))
+                GUILayout.Label("Avatar Material Configuration", EditorStyles.boldLabel);
+                foreach (var avatar in avatars)
                 {
-                    searchResults.Clear();
-                }
-                else
-                {
-                    SearchObjectsWithMaterials();
-                }
-            }
-
-            // 置き換え先マテリアルを選択するフィールド
-            replaceMaterial = (Material)EditorGUILayout.ObjectField("Replace Material", replaceMaterial, typeof(Material), false);
-
-            // 再検索ボタン
-            if (GUILayout.Button("Reload Search Results"))
-            {
-                SearchObjectsWithMaterials();
-            }
-
-            // スクロールビューの開始
-            scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
-
-            if (searchResults.Count > 0)
-            {
-                DisplaySearchResults();
-            }
-
-            // スクロールビューの終了
-            EditorGUILayout.EndScrollView();
-
-            // 追加したチェックボックスを表示
-            switchMaterialsAfterReplace = EditorGUILayout.Toggle("Switch After Replace", switchMaterialsAfterReplace);
-
-            // 置換ボタンの表示と有効化設定
-            GUI.enabled = replaceMaterial != null;
-            if (GUILayout.Button("Replace"))
-            {
-                ReplaceMaterials();
-            }
-            GUI.enabled = true;
-        }
-
-        /// <summary>
-        /// 指定されたマテリアルを持つゲームオブジェクトを検索する
-        /// </summary>
-        private void SearchObjectsWithMaterials()
-        {
-            if (targetMaterials.Count == 0) return;
-
-            // ヒエラルキー内のすべてのゲームオブジェクトを検索し、指定されたマテリアルを持つものを抽出する
-            searchResults = GameObject.FindObjectsOfType<GameObject>(true)
-                .Where(gameObject => gameObject.hideFlags == HideFlags.None)
-                .Where(gameObject => gameObject.GetComponent<Renderer>() != null)
-                .Where(gameObject => gameObject.GetComponent<Renderer>().sharedMaterials.Intersect(targetMaterials).Any())
-                .GroupBy(gameObject => gameObject.transform.root.gameObject)
-                .ToDictionary(group => group.Key, group => group.ToList());
-
-            // 各オブジェクトのトグル状態を初期化
-            toggles = searchResults.Keys.ToDictionary(key => key, key => true);
-            objectToggles = searchResults.Values.SelectMany(list => list).ToDictionary(obj => obj, obj => true);
-        }
-
-        /// <summary>
-        /// 検索結果を表示する
-        /// </summary>
-        private void DisplaySearchResults()
-        {
-            foreach (var (root, resultMeshes) in searchResults)
-            {
-                EditorGUILayout.Space();
-                EditorGUILayout.BeginVertical(BoxStyle);
-
-                // ルートオブジェクトのトグルと表示
-                EditorGUILayout.BeginHorizontal();
-                toggles[root] = EditorGUILayout.Toggle(toggles[root], GUILayout.Width(15));
-                CreateReadonlyObjectField("", root);
-                EditorGUILayout.EndHorizontal();
-
-                if (toggles[root])
-                {
-                    // コンテンツの垂直レイアウト
-                    EditorGUILayout.BeginVertical(ContentStyle);
-
-                    foreach (var mesh in resultMeshes)
+                    if (avatar != null)
                     {
-                        EditorGUILayout.BeginHorizontal();
-                        objectToggles[mesh] = EditorGUILayout.Toggle(objectToggles[mesh], GUILayout.Width(15));
-                        CreateReadonlyObjectField("", mesh);
-                        EditorGUILayout.EndHorizontal();
+                        DrawAvatarMaterialConfiguration(avatar);
                     }
-
-                    EditorGUILayout.EndVertical();
                 }
+            }
+            EditorGUILayout.EndScrollView();
+        }
 
-                EditorGUILayout.EndVertical();
+        private void DrawAvatarMaterialConfiguration(VRCAvatarDescriptor avatar)
+        {
+            using (new EditorGUILayout.VerticalScope("box"))
+            {
+                EditorGUI.BeginDisabledGroup(true);
+                EditorGUILayout.ObjectField(avatar.gameObject.name, avatar, typeof(VRCAvatarDescriptor), false);
+                EditorGUI.EndDisabledGroup();
+
+                Dictionary<GameObject, List<Material>> materialData = AvatarMaterialConfiguration.ExtractMaterialData(avatar.gameObject);
+                AvatarMaterialConfiguration avatarMaterialConfig = new AvatarMaterialConfiguration(avatar.gameObject, materialData);
+
+                foreach (var material in avatarMaterialConfig.Materials.Keys)
+                {
+                    using (new EditorGUI.IndentLevelScope())
+                    using (new EditorGUILayout.VerticalScope("box"))
+                    {
+
+                        using (new EditorGUILayout.HorizontalScope())
+                        {
+                            EditorGUI.BeginDisabledGroup(true);
+                            EditorGUILayout.ObjectField(material, typeof(Material), false);
+                            EditorGUI.EndDisabledGroup();
+
+                            EditorGUILayout.LabelField("→", GUILayout.Width(40));
+
+                            Material targetMaterial = (Material)EditorGUILayout.ObjectField(null, typeof(Material), false);
+                        }
+                        using (new EditorGUI.IndentLevelScope())
+                        {
+                            foreach (var location in avatarMaterialConfig.Materials[material])
+                            {
+                                using (new EditorGUILayout.HorizontalScope())
+                                {
+                                    bool isSelected = EditorGUILayout.Toggle(true, GUILayout.Width(20));
+                                    EditorGUI.BeginDisabledGroup(true);
+                                    EditorGUILayout.ObjectField(location.Mesh, typeof(GameObject), true);
+                                    EditorGUI.EndDisabledGroup();
+                                }
+                            }
+                        }
+
+                    }
+                }
             }
         }
 
-        /// <summary>
-        /// 選択されたオブジェクトのマテリアルを置き換える
-        /// </summary>
-        private void ReplaceMaterials()
+        private void HandleDragAndDrop(Rect dropArea)
         {
-            // Undoグループの開始
-            Undo.IncrementCurrentGroup();
-            Undo.SetCurrentGroupName("Replace Materials");
+            Event evt = Event.current;
 
-            // 置換前のマテリアルを保存
-            Material previousReplaceMaterial = replaceMaterial;
-
-            // 検索結果の各オブジェクトのマテリアルを置き換える
-            var replaceTargetRenderer = searchResults
-                .Where(kv => toggles[kv.Key]) // トグルが true のオブジェクトのみに絞る
-                .SelectMany(kv => kv.Value)
-                .Where(obj => objectToggles[obj]) // 個々のオブジェクトのチェック状態も確認
-                .Select(obj => obj.GetComponent<Renderer>())
-                .Where(renderer => renderer != null);
-
-            foreach (var renderer in replaceTargetRenderer)
+            switch (evt.type)
             {
-                // Rendererの状態をUndoの対象として記録
-                Undo.RecordObject(renderer, "Replace Material");
+                case EventType.DragUpdated:
+                case EventType.DragPerform:
+                    if (!dropArea.Contains(evt.mousePosition))
+                        return;
 
-                // マテリアルを置き換える
-                renderer.sharedMaterials = renderer.sharedMaterials
-                    .Select(material => targetMaterials.Contains(material) ? replaceMaterial : material)
-                    .ToArray();
+                    DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
+
+                    if (evt.type == EventType.DragPerform)
+                    {
+                        DragAndDrop.AcceptDrag();
+
+                        foreach (Object draggedObject in DragAndDrop.objectReferences)
+                        {
+                            VRCAvatarDescriptor avatar = ((GameObject)draggedObject)?.GetComponent<VRCAvatarDescriptor>();
+                            if (avatar != null && !avatars.Contains(avatar))
+                            {
+                                avatars.Add(avatar);
+                            }
+                        }
+                    }
+                    Event.current.Use();
+                    break;
             }
-
-            // Undoグループを一つの操作としてまとめる
-            Undo.CollapseUndoOperations(Undo.GetCurrentGroup());
-
-            // 置換後の設定を更新する機能が有効な場合
-            if (switchMaterialsAfterReplace)
-            {
-                // 置き換えマテリアルをNoneに設定
-                replaceMaterial = null;
-
-                // 対象マテリアルを先ほどの置換マテリアル1つに設定
-                targetMaterials.Clear();
-                targetMaterials.Add(previousReplaceMaterial);
-
-                // シリアライズされたオブジェクトに変更を適用
-                serializedObject.Update();
-                serializedObject.ApplyModifiedProperties();
-            }
-
-            // 検索を再実行して最新の状態を反映
-            SearchObjectsWithMaterials();
-        }
-
-        /// <summary>
-        /// 読み取り専用のオブジェクトフィールドを作成する
-        /// </summary>
-        /// <param name="label">オブジェクトフィールドのラベル</param>
-        /// <param name="obj">表示するオブジェクト</param>
-        private void CreateReadonlyObjectField(string label, Object obj)
-        {
-            GUI.enabled = false;
-            EditorGUILayout.ObjectField(label, obj, typeof(GameObject), false);
-            GUI.enabled = true;
         }
     }
 }
