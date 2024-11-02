b2133ca91a56c7547d159897c8ba6ccd129b675f
Anosion <anosion.vr@gmail.com>
2024-11-02 15:13:44 +0900

    表示用にマテリアルのパスによるグルーピングを追加

--- Editor/AvatarMaterialConfiguration.cs
+++ Editor/AvatarMaterialConfiguration.cs
@@ -1,17 +1,17 @@
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
 using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace Anosion.MaterialReplacer
 {
     public class AvatarMaterialConfiguration
     {
-        private readonly Dictionary<Material, List<MaterialLocation>> materials;
-
         public GameObject Avatar { get; }
-        public ImmutableDictionary<Material, ImmutableArray<MaterialLocation>> Materials =>
-            materials.ToImmutableDictionary(entry => entry.Key, entry => ImmutableArray.CreateRange(entry.Value));
+        public Dictionary<Material, List<MaterialLocation>> Materials { get; }
+        public SortedDictionary<string, SortedDictionary<Material, List<MaterialLocation>>> MaterialGroups { get; }
 
         public AvatarMaterialConfiguration(GameObject avatar, Dictionary<GameObject, List<Material>> objectMaterialData) : this(
             avatar,
@@ -28,14 +28,22 @@ namespace Anosion.MaterialReplacer
         private AvatarMaterialConfiguration(GameObject avatar, Dictionary<Material, List<MaterialLocation>> materials)
         {
             Avatar = avatar;
-            this.materials = materials;
+            Materials = materials;
+
+            var materialPaths = Materials.Keys.ToDictionary(material => material, AssetDatabase.GetAssetPath);
+            MaterialGroups = new SortedDictionary<string, SortedDictionary<Material, List<MaterialLocation>>>(Materials
+                .GroupBy(materialEntry => Regex.Replace(Path.GetDirectoryName(materialPaths[materialEntry.Key]), "^Assets.", ""))
+                .ToDictionary(
+                    group => group.Key,
+                    group => new SortedDictionary<Material, List<MaterialLocation>>(group.ToDictionary(pair => pair.Key, pair => pair.Value), new MaterialPathComparer(materialPaths))
+                ));
         }
 
         public AvatarMaterialConfiguration TransformMaterials(Dictionary<Material, Material> replacementMap, Dictionary<MaterialLocation, bool> selectedMeshLocations)
         {
             bool isMeshSelected(MaterialLocation location) => selectedMeshLocations.TryGetValue(location, out var isSelected) && isSelected;
 
-            var newMaterials = materials
+            return new AvatarMaterialConfiguration(Avatar, Materials
                 .SelectMany(materialEntry => materialEntry.Value.Select(matLocation => (
                     Material: isMeshSelected(matLocation) && replacementMap.TryGetValue(materialEntry.Key, out var replacement) && replacement != null
                         ? replacement
@@ -47,14 +55,12 @@ namespace Anosion.MaterialReplacer
                 .ToDictionary(
                     group => group.Key,
                     group => group.Select(entry => new MaterialLocation(entry.Mesh, entry.SlotIndex)).ToList()
-                );
-
-            return new AvatarMaterialConfiguration(Avatar, newMaterials);
+                ));
         }
 
         public bool HasDifferences(Dictionary<GameObject, List<Material>> objectMaterialData)
         {
-            Dictionary<GameObject, List<Material>> thisObjectMaterialData = materials
+            Dictionary<GameObject, List<Material>> thisObjectMaterialData = Materials
                 .SelectMany(entry => entry.Value.Select(location => (location.Mesh, Material: entry.Key, location.SlotIndex)))
                 .GroupBy(config => config.Mesh)
                 .ToDictionary(
@@ -69,26 +75,19 @@ namespace Anosion.MaterialReplacer
                    thisObjectMaterialData.Any(kvp => kvp.Value.Count != objectMaterialData[kvp.Key].Count || !kvp.Value.SequenceEqual(objectMaterialData[kvp.Key]));
         }
 
-        public static Dictionary<GameObject, List<Material>> ExtractMaterialData(GameObject avatar)
-        {
-            return avatar.GetComponentsInChildren<Renderer>()
+        public static Dictionary<GameObject, List<Material>> ExtractMaterialData(GameObject avatar) =>
+            avatar.GetComponentsInChildren<Renderer>()
                 .Where(renderer => renderer != null)
                 .ToDictionary(renderer => renderer.gameObject, renderer => renderer.sharedMaterials.ToList());
-        }
 
         public static void ApplyMaterials(AvatarMaterialConfiguration config)
         {
-            foreach (var materialEntry in config.materials)
+            foreach (var (materialEntry, location) in config.Materials.SelectMany(materialEntry =>
+                materialEntry.Value
+                    .Where(location => materialEntry.Key != null && location != null)
+                    .Select(location => (materialEntry, location))))
             {
-                Material material = materialEntry.Key;
-
-                foreach (var location in materialEntry.Value)
-                {
-                    if (material != null && location != null)
-                    {
-                        location.ApplyMaterial(material);
-                    }
-                }
+                location.ApplyMaterial(materialEntry.Key);
             }
         }
 
@@ -119,6 +118,25 @@ namespace Anosion.MaterialReplacer
                         renderer.sharedMaterials = materials;
                     }
                 }
+            }
+        }
+
+        private class MaterialPathComparer : IComparer<Material>
+        {
+            private Dictionary<Material, string> PathOf { get; }
+
+            public MaterialPathComparer(Dictionary<Material, string> pathOf)
+            {
+                PathOf = pathOf;
+            }
+
+            public int Compare(Material x, Material y)
+            {
+                if (x == null && y == null) return 0;
+                if (x == null) return -1;
+                if (y == null) return 1;
+
+                return string.Compare(PathOf[x], PathOf[y], System.StringComparison.Ordinal);
             }
         }
     }
